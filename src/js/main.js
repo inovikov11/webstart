a89ce970c2d465fbea173341379a8dfbd7a9d9f0
inovikov11 <igor.niko.novikov@mail.ru>
2020-05-11 02:10:19 +0300

    Сверстан и адаптирован слайдер на owlcarousel

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,12 @@
 $(function(){
+  //Подключаем модальное окно
   $('#button').click(function() {
     $('.modal').toggleClass('modal_active');
   });
   $('#close-button').click(function() {
     $('.modal').toggleClass('modal_active');
   });
+  //Подключаем стрелку вверх
   $(window).scroll(function() {
     if($(this).scrollTop() != 0) {
       $('#arrow').fadeIn();
@@ -14,5 +16,25 @@ $(function(){
   });
     $('#arrow').click(function() {
     $('body,html').animate({scrollTop:0},800);
+  });
+  //Подключаем слайдер
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    autoWidth:false,
+    center:false,
+    nav:true,
+    navText:[$('.arrow-left'), $('.arrow-right')],
+    margin:10,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1200:{
+            items:3
+        }
+    }
   });
 });
